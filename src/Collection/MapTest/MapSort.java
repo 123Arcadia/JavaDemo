73363fb125c55eb795e4751543fde67f6939de44
123Arcadia <1119045415@qq.com>
2023-09-29 16:53:11 +0800

    sortedMap、TreeMap以value排序

--- src/Collection/MapTest/MapSort.java
+++ src/Collection/MapTest/MapSort.java
@@ -84,6 +84,12 @@ public class MapSort {
     @Test
     public void test_Entry_sort() {
         Map<Integer , Integer> map = new TreeMap<>(new Comparator<Integer>() {
+            /**
+             * 倒序排列
+             * @param o1 the first object to be compared.
+             * @param o2 the second object to be compared.
+             * @return
+             */
             @Override
             public int compare(Integer o1, Integer o2) {
                 return -Integer.compare(o1, o2);
@@ -103,8 +109,72 @@ public class MapSort {
         List<Integer> list = new ArrayList<>(entrySet);
         System.out.println("list = " + list);
         System.out.println(list.get(0));
-
+        //list = [66=1, 7=7, 6=6, 5=5, 4=4, 3=3, 2=2]
+        //66=1
     }
 
+    /**
+     * TreeMap: 以value值排序
+     */
+    @Test
+    public void sortMapReversebyValue() {
+        SortedMap<String,String> sortedMap = new TreeMap<String,String>();
+        sortedMap.put("1", "a");
+        sortedMap.put("5", "b");
+        sortedMap.put("2", "c");
+        sortedMap.put("4", "d");
+        sortedMap.put("3", "e");
+        System.out.println("(sort before)sortedMap = " + sortedMap); // sortedMap还是不变
 
+        //sortedMap:1->a
+        //sortedMap:2->c
+        //sortedMap:3->e
+        //sortedMap:4->d
+        //sortedMap:5->b
+
+        Set<Map.Entry<String, String>> entries = sortedMap.entrySet();
+        ArrayList<Map.Entry<String, String>> list = new ArrayList<>(entries);
+        Collections.sort(list, new Comparator<Map.Entry<String, String>>() {
+            @Override
+            public int compare(Map.Entry<String, String> o1, Map.Entry<String, String> o2) {
+                // 升序
+                // o1在前，o2在后
+                // -1 -> <
+//                return o1.getValue().compareTo(o2.getValue());
+                return o1.getValue().compareTo(o2.getValue());
+            }
+        });
+        System.out.println("(sort after)sortedMap = " + sortedMap); // sortedMap还是不变
+        //sortedMap = {1=a, 2=c, 3=e, 4=d, 5=b}
+        System.out.println("(sort after)list = " + list); // 但是list排序变了
+        //list = [1=a, 5=b, 2=c, 4=d, 3=e]
+    }
+    @Test
+    public void treeMapReversebyValue() {
+        Map<String,String> treeMpa = new TreeMap<String,String>();
+        treeMpa.put("1", "a");
+        treeMpa.put("5", "b");
+        treeMpa.put("2", "c");
+        treeMpa.put("4", "d");
+        treeMpa.put("3", "e");
+        System.out.println("(sort before)treeMpa = " + treeMpa); // sortedMap还是不变
+//        (sort before)treeMpa = {1=a, 2=c, 3=e, 4=d, 5=b}
+
+        Set<Map.Entry<String, String>> entries = treeMpa.entrySet();
+        ArrayList<Map.Entry<String, String>> list = new ArrayList<>(entries);
+        Collections.sort(list, new Comparator<Map.Entry<String, String>>() {
+            @Override
+            public int compare(Map.Entry<String, String> o1, Map.Entry<String, String> o2) {
+                // 升序
+                // o1在前，o2在后
+                // -1 -> <
+//                return o1.getValue().compareTo(o2.getValue());
+                return o1.getValue().compareTo(o2.getValue());
+            }
+        });
+        System.out.println("(sort after)treeMpa = " + treeMpa); // sortedMap还是不变
+        //(sort after)treeMpa = {1=a, 2=c, 3=e, 4=d, 5=b}
+        System.out.println("(sort after)list = " + list); // 但是list排序变了
+        //(sort after)list = [1=a, 5=b, 2=c, 4=d, 3=e]
+    }
 }
