c381fc7032835a47b7cd83066e98aac99b7d26d4
123Arcadia <1119045415@qq.com>
2023-11-17 18:27:06 +0800

    List.Of 和 arraysAsList 不可 add ListOf 不可以set 修改

--- src/Collection/List/ListArrayasList.java
+++ src/Collection/List/ListArrayasList.java
@@ -114,6 +114,28 @@ public class ListArrayasList {
 
     }
 
+    @Test
+    public void DequeueTest() {
+        Deque<Integer> deque = new ArrayDeque<>();
+        deque.add(1);
+        deque.add(4);
+        deque.add(2);
+        System.out.println("deque = " + deque); // deque = [1, 4, 2]
+        System.out.println("deque.size() = " + deque.size()); // deque = 3
+        Integer pop = deque.pop();
+        System.out.println("pop = " + pop); // 1
+        /**
+         * 此时 11 会加在前面，因为 push 适应的是 栈
+         */
+        deque.push(11);
+        System.out.println("deque.getFirst() = " + deque.getFirst()); // 11
+        System.out.printf("%-5d\n", deque.getLast()); // 2
+        boolean offerFirst = deque.offerFirst(2);
+        System.out.println("offerFirst = " + offerFirst); // true
+        System.out.println("deque = " + deque); // deque = [2, 11, 4, 2]
+    }
+
+
     /**
      * List.Of 和 arraysAsList 不可 add
      * ListOf 不可以set 修改
@@ -130,8 +152,5 @@ public class ListArrayasList {
         arraysAsList.set(1, "zc3");
         System.out.println("ListOf:" + ListOf + "\n" + "arraysAsList:" + arraysAsList);
     }
-
-
-
 
 }
