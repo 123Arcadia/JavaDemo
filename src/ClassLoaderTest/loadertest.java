864e9938161898a60de29cd38d885e644f1b63d9
123Arcadia <1119045415@qq.com>
2023-12-31 22:48:17 +0800

    class和classLoader使用getResource()和getResourceAstream()

--- src/ClassLoaderTest/loadertest.java
+++ src/ClassLoaderTest/loadertest.java
@@ -1,12 +1,10 @@
 package ClassLoaderTest;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.junit.Test;
 
+import java.io.FileInputStream;
 import java.io.InputStream;
-import java.util.*;
+import java.util.Properties;
 
 public class loadertest {
 
@@ -29,6 +27,14 @@ public class loadertest {
         System.out.println(classLoader2);
         ClassLoader classLoader3 = String.class.getClassLoader();
         System.out.println(classLoader3);
+        //jdk.internal.loader.ClassLoaders$AppClassLoader@63947c6b
+        //jdk.internal.loader.ClassLoaders$PlatformClassLoader@28f67ac7
+        //null
+        //null
+
+        String classPath = System.getProperty("java.class.path");
+        System.out.println("classPath = " + classPath);
+        //classPath = D:\javaIDEA\IntelliJ IDEA 2022.2.1\lib\idea_rt.jar;D:\javaIDEA\IntelliJ IDEA 2022.2.1\plugins\junit\lib\junit5-rt.jar;D:\javaIDEA\IntelliJ IDEA 2022.2.1\plugins\junit\lib\junit-rt.jar;D:\javaProject\javaTesting\out\production\javaTesting;D:\javaProject\javaTesting\lib\junit-4.13.1.jar;D:\javaProject\javaTesting\lib\hamcrest-core-1.3.jar;D:\javaProject\javaTesting\lib\lombok-1.18.22.jar;D:\CSProject\maven-Repo\repository\org\slf4j\slf4j-api\1.7.25\slf4j-api-1.7.25.jar
     }
 
 
@@ -37,103 +43,72 @@ public class loadertest {
      */
     @Test
     public void test2() throws Exception {
+        System.out.println(System.getProperty("user.dir"));
+        System.out.println(loadertest.class.getClassLoader().getResource(""));
+        System.out.println(loadertest.class.getClassLoader().getResourceAsStream("").toString());
+        //D:\javaProject\javaTesting
+        //file:/D:/javaProject/javaTesting/out/production/javaTesting/
+        //java.io.ByteArrayInputStream@67b64c45
+
+
         Properties pros = new Properties();
         //此时的文件默认在当前的module下。
         //读取配置文件的方式一：
-//        FileInputStream fis = new FileInputStream("jdbc.properties");
+//        FileInputStream fis = new FileInputStream(System.getProperty("user.dir") + "/src/ClassLoaderTest"+"/test.properties");
 //        pros.load(fis);
+
         //读取配置文件的方式二：使用ClassLoader
         //配置文件默认识别为：当前module的src下
         ClassLoader classLoader = loadertest.class.getClassLoader();
         /**
          * 要创建properties文件
          */
-        InputStream is = classLoader.getResourceAsStream("jdbc1.properties");
+        InputStream is = classLoader.getResourceAsStream("ClassLoaderTest"+"/test.properties");
         pros.load(is);
         String user = pros.getProperty("user");
         String password = pros.getProperty("password");
         System.out.println("user = " + user + ",password = " + password);
+        //user = zcw,password = zcw123
     }
 
+    /**
+     *	- ClassLoader来说：不能使用"/"开头，默认从ClassPath类路径下获取
+     * 	- class来说：
+     * 	        path不以’/‘开头时，默认是指所在类的相对路径，从这个相对路径下取资源；
+     *          path以’/'开头时，则是从项目的ClassPath根下获取资源，就是要写相对于classpath类路径下的绝对路径
+     */
     @Test
-    public void css() {
-        List<Integer> list = new ArrayList<Integer>() {{
-            add(2);
-            add(4);
-            add(4);
-            add(8);
-        }};
-        System.out.println("list = " + list);
-        System.out.println(list.indexOf(4)); // 1
-        System.out.println(list.lastIndexOf(4)); // 2
+    public void testgetResource() {
+        System.out.println(System.getProperty("user.dir"));
+        //D:\javaProject\javaTesting
+        System.out.println(loadertest.class.getResource(""));
+        //file:/D:/javaProject/javaTesting/out/production/javaTesting/ClassLoaderTest/
+        /**
+         * /: 表示从根目录下
+         * "": 表示从该类下
+         */
+        System.out.println(loadertest.class.getResource("/"));
+        //file:/D:/javaProject/javaTesting/out/production/javaTesting/
+        System.out.println(loadertest.class.getResource("").getFile());
+        ///D:/javaProject/javaTesting/out/production/javaTesting/ClassLoaderTest/
+        System.out.println(loadertest.class.getResourceAsStream(""));
+        //java.io.ByteArrayInputStream@67b64c45
+        System.out.println(loadertest.class.getResourceAsStream("/"));
+        //java.io.ByteArrayInputStream@4411d970
 
-        List<Integer> list1 = new ArrayList<>();
-        list1.add(1);
-        System.out.println("list1 = " + list1);
-
-        System.out.println(Math.ceil((double) 72 / 22));
-
-        int o = 0;
-        for (int i = 1; i <= 91; i++) {
-            if (i % 2 != 1) {
-                o ++;
-            }
+        Properties pros = new Properties();
+        String file = loadertest.class.getResource("/ClassLoaderTest/test.properties").getFile();
+        FileInputStream fis = null;
+        try {
+            fis = new FileInputStream(file);
+            pros.load(fis);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        System.out.println("o = " + o);
+        String user = pros.getProperty("user");
+        String password = pros.getProperty("password");
+        System.out.println("user = " + user + ",password = " + password);
     }
-
-    @Test
-    public void testStu() {
-        stu zcw1 = new stu("zcw1",16 );
-        stu zcw2 = new stu("zcw2", 15);
-        List<stu> list =new ArrayList<>();
-        list.add(zcw1);
-        list.add(zcw2);
-        System.out.println(list);
-//        System.out.println(zcw1.compareTo(zcw2));
-
-        Collections.sort(list, (o1, o2) -> o1.age.compareTo(o2.age));
-        System.out.println(list);
-    }
-
-    @Data
-    @AllArgsConstructor
-    @NoArgsConstructor
-    class stu{
-        private String name;
-        private Integer age;
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-
-            stu stu = (stu) o;
-
-            if (!name.equals(stu.name)) return false;
-            return age.equals(stu.age);
-        }
-
-
-        @Override
-        public int hashCode() {
-            int result = name.hashCode();
-            result = 31 * result + age.hashCode();
-            return result;
-        }
-
-//        @Override
-//        public int compareTo(stu o) {
-//            if (this.age < o.age) {
-//                return -1;
-//            } else if (this.age > o.age) {
-//                return 1;
-//            }
-//            return 0;
-//        }
-    }
-
-
 
 
 }
